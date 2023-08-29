a8ec4ec75ac7637cec9ce93054607b9470c91eec
Joel Canté <97580350+JoelCante@users.noreply.github.com>
2023-08-28 22:37:59 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
-const INITIAL_STONES_COUNT = 4;
+function iniciar(){
+ emergente();
+ iniciaJuego();
+}
+
+function abrir(){
+document.getElementById("reg").style.display="block";
+
+}
+
+function cerrar(){
+  document.getElementById("reg").style.display="none";
+  document.getElementById("cont_em").style.display="none";
+  document.getElementById("cont_tur").style.display="none";
+  document.getElementById("cont_cap").style.display="none";
+  }
+
+  function cerrar2(){
+    document.getElementById("gan").style.display="none";
+    iniciaJuego();
+    document.getElementById("gan2").style.display="none";
+    iniciaJuego();
+    }
+
+ function emergente(){
+  document.getElementById("cont_em").style.display="block";
+ }   
+
+
+
+
+const FICHAS_CONT = 4;
 let Jug = {
-  0: { Fch: [], score: 0 },
+  0: { Fch: [], score: 0 }, ///LA VARIABLE "PUNT" genera conflicto con el HTML
   1: { Fch: [], score: 0 },
 };
 
 let Tur;
-let turnText = document.querySelector(".Texto_turno");
+let turnText = document.querySelector("#turn");
 
-let jug1scoreText = document.querySelector(".contenedorjug1");
-let jug2scoreText = document.querySelector(".contenedorjug2");
+let jug1scoreText = document.querySelector("#player1score");
+let jug2scoreText = document.querySelector("#player2score");
 
 let table = document.querySelector("table");
 let Filas = [];
-Filas[0] = document.querySelector(".Fila-Jug1");
-Filas[1] = document.querySelector(".Fila-Jug2");
+Filas[0] = document.querySelector("#player1stones");
+Filas[1] = document.querySelector("#player2stones");
 
 let body = document.querySelector("body");
 
 iniciaJuego();
 function iniciaJuego() {
+
   for (let i = 0; i < 2; i++) {
     Jug[i].score = 0;
     for (let j = 0; j < 6; j++) {
-      Jug[i].Fch[j] = INITIAL_STONES_COUNT;
+      Jug[i].Fch[j] = FICHAS_CONT;
     }
   }
   Tur = 1;
   updateTabla();
-  div.addEventListener("click", play);
+  table.addEventListener("click", play);
 }
 
 function updateTabla() {
@@ -36,6 +68,13 @@ function updateTabla() {
       Filas[i].children[j].innerHTML = Jug[i].Fch[j];
     }
   turnText.innerHTML = `Es Turno del Jugador ${Tur} `;
+  if(Tur==1){
+    document.querySelector("#jug1").style.border = "2px solid #ECE632";
+    document.querySelector("#jug2").style.border = "none";
+  } else{
+    document.querySelector("#jug2").style.border = "2px solid #ECE632";
+    document.querySelector("#jug1").style.border = "none";
+  }
   jug1scoreText.innerHTML = Jug[0].score;
   jug2scoreText.innerHTML = Jug[1].score;
 }
@@ -82,9 +121,9 @@ function updateCell(row, cell, timer, lastStone) {
         updateTabla();
         cleanEstilo();
         table.addEventListener("click", play);
-      }, 400);
+      }, 300);
     }
-  }, 400 * timer);
+  }, 300 * timer);
 }
 
 function finalJuego() {
@@ -92,7 +131,7 @@ function finalJuego() {
   let clearLine;
   let ganador;
   for (let i = 0; i < 6; i++) {
-    if (Jug[0].stones[i] != 0) finJuego = false;
+    if (Jug[0].Fch[i] != 0) finJuego = false;
   }
   clearLine = finJuego ? 0 : 1;
   otherPlayer = clearLine == 0 ? 1 : 0;
@@ -109,16 +148,16 @@ function finalJuego() {
       turnText.innerHTML = `Empate!!!.`;
     else {
       ganador = Jug[0].score > Jug[1].score ? 1 : 2;
-      turnText.innerHTML = `Enhorabuena Jugador ${winner} ganaste!!.`;
+    
+      if(ganador==1){
+        document.getElementById("gan").style.display="block";
+      }else{
+        document.getElementById("gan2").style.display="block";
+       
+      }
+
     }
     table.removeEventListener("click", play);
-    button = document.createElement("button");
-    button.innerHTML = "Nuevo Juego";
-    button.addEventListener(`click`, () => {
-      iniciaJuego();
-      body.removeChild(button);
-    });
-    body.appendChild(button);
   }
 }
 
@@ -144,54 +183,68 @@ turnText.addEventListener("click", function () {
   }
 });
 
+function minmax(){
+let movimiento = play(e);
+
+if(movimiento){
+  return true;
+ }
+}
+
+
+
+
 function play(e) {
   let cell = e.target.cellIndex;
-  let row = e.target.parentNode.rowIndex;
+  let Pos = e.target.parentNode.rowIndex;
   let direction = 0;
 
-  if (Jug[row].Fch[cell] > 0 && row == Tur - 1) {
-    // A good click
+  if (Jug[Pos].Fch[cell] > 0 && Pos == Tur - 1) {
+    //Iniciando con un Clic  
     table.removeEventListener("click", play);
-    let currentMoveFch = Jug[row].Fch[cell];
+    let currentMoveFch = Jug[Pos].Fch[cell];
     let initialFch = currentMoveFch;
-    let initialCell = `row: ${row} cell: ${cell}`;
+    let initialCell = `row: ${Pos} cell: ${cell}`;
     let timer = 0;
-    Jug[row].Fch[cell] = 0;
-    direction = row == 0 ? -1 : 1;
-    Tur = row == 0 ? 2 : 1;
-    currentPlayer = row == 0 ? 0 : 1;
-    otherPlayer = row == 0 ? 1 : 0;
-    updateCell(row, cell, timer, currentMoveFch);
+    Jug[Pos].Fch[cell] = 0;
+    direction = Pos == 0 ? -1 : 1;
+    Tur = Pos == 0 ? 2 : 1;
+    currentPlayer = Pos == 0 ? 0 : 1;
+    otherPlayer = Pos == 0 ? 1 : 0;
+    updateCell(Pos, cell, timer, currentMoveFch);
     cell += direction;
 
     while (currentMoveFch > 0) {
       timer++;
       if (
-        currentMoveFch == 1 && //when finish on empty cell
-        currentPlayer == row &&
+        currentMoveFch == 1 && //Cuando finaliza deja  en 0
+        currentPlayer == Pos &&
         Jug[currentPlayer].Fch[cell] == 0
       ) {
          Jug[currentPlayer].score += Jug[otherPlayer].Fch[cell] + 1;
         Jug[otherPlayer].Fch[cell] = 0;
-        updateCell(row, cell, timer, currentMoveFch);
+        updateCell(Pos, cell, timer, currentMoveFch);
         updateCell(otherPlayer, cell, timer, currentMoveFch);
+        
+        document.getElementById("cont_cap").style.display="block";
       } else if (cell >= 0 && cell <= 5 && currentMoveFch > 0) {
-        // run the row
-        Jug[row].Fch[cell]++;
-        updateCell(row, cell, timer, currentMoveFch);
+        // Recorre las posciciones
+        Jug[Pos].Fch[cell]++;
+        updateCell(Pos, cell, timer, currentMoveFch);
       } else if (cell == -1 || cell == 6) {
-        // when gets to a player cell
-        if (currentPlayer == row) {
+        // Obtiene las celda del otro jugador
+        if (currentPlayer == Pos) {
           Jug[currentPlayer].score++;
-          updateCell(row, cell, timer, currentMoveFch);
+          updateCell(Pos, cell, timer, currentMoveFch);
           if (currentMoveFch == 1) {
             Tur = Tur == 1 ? 2 : 1;
-          }
+           document.getElementById("cont_tur").style.display="block";
+          }  
         } else {
           currentMoveFch++;
         }
-        row = row == 0 ? 1 : 0;
-        direction = row == 0 ? -1 : 1;
+        Pos = Pos == 0 ? 1 : 0;
+        direction = Pos == 0 ? -1 : 1;
       }
       cell += direction;
       currentMoveFch--;
@@ -199,6 +252,10 @@ function play(e) {
     }
     checkGame();
     finalJuego();
+   
     
+
+
   }
 }
+
